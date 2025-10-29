36cf73b3c1b7a4dbd354503db908f2aa13cda42a
raccrank <raccrank@gmail.com>
2025-10-29 19:03:13 +0300

    Update app.py
    changed app.run, last line

--- app.py
+++ app.py
@@ -292,5 +292,6 @@ def webhook():
     return str(resp)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    init_db() 
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
